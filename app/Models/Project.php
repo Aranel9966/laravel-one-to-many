2d73aaa5036a9cecfd76b8205dd31bb3531dfb76
Aranel9966 <99angelo.dire@gmail.com>
2023-05-23 21:33:53 +0200

    add migration and models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'thumb'];
+    protected $fillable = ['title', 'description', 'thumb', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
